e006b11ff83145d90df54f16961e9bf64ce2da1e
jungwone <30407259+jungwone@users.noreply.github.com>
2022-07-09 22:46:49 +0900

    Time: 343 ms (32.38%), Space: 59.9 MB (33.43%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[][]}
+ */
+var updateMatrix = function(mat) {
+    const visited = 
+          Array.from(new Array(mat.length), () => new Array(mat[0].length).fill(false)); // 방문 여부 표시 배열
+    const m = mat.length;
+    const n = mat[0].length;
+    const queue = [];
+    
+    for(let i = 0; i < mat.length; i++) {
+        for(let j = 0; j < mat[0].length; j++) {
+            if(mat[i][j] === 0) queue.push([i, j]);
+            else mat[i][j] = Infinity; // 거리를 구하기 전이므로 무한으로 설정
+        }
+    }
+    
+    const direction = [[-1, 0], [1, 0], [0, 1], [0, -1]];
+    
+    while(queue.length) {
+        const [x, y] = queue.shift();
+        visited[x,y] = true;
+        direction.forEach(([ax, ay]) => {
+            ax += x;
+            ay += y;
+            
+            if(ax < 0 || ax >= m || ay < 0 || ay >= n) return;
+            if(visited[ax][ay]) return;
+            if(mat[ax][ay] < mat[x][y] + 1) return;
+            
+            mat[ax][ay] = mat[x][y] + 1;
+            queue.push([ax,ay])
+        });
+    }
+    
+    return mat;
+};
